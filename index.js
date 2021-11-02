f3edc3f44e8c251d09af9e9406ebc63efaaf4169
imtripleq <flamework89@hotmail.com>
2021-11-03 07:45:44 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+// const { FetchError } = require("node-fetch");
+
+function submitData(user, address) {
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: user,
+      email: address,
+    }),
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      document.body.innerHTML = data.name;
+      document.body.innerHTML = data.email;
+      document.body.innerHTML = data.id;
+    })
+    .catch((err) => {
+      document.body.innerHTML = err.message;
+      document.body.innerHTML = "Unauthorized Access";
+      throw new FetchError("Unauthorized Access");
+    });
+}
